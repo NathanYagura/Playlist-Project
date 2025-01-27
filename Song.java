acb8721fbfbbf9125e8ff88f98358f1ff8925f0b
Nathan Yagura <nyagura27@students.polytechnic.org>
2025-01-27 08:54:16 -0800

    lucas this is what we do

--- Song.java
+++ Song.java
@@ -45,23 +45,6 @@ public class Song {
       public Boolean getLiked(){
         return liked;
       }
-      public void Like(){
-        liked = true;
-      }
-
-      public int getSeconds(){
-        return seconds%60;
-      }
-      public int getMinutes(){
-        return seconds/60;
-      }
-      public int totalTime(){
-        return seconds;
-      }
-      public void like(){
-        liked = true;
-      }
-
       public int getSeconds(){
         return seconds%60;
       }
